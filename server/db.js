f6e19585014b6de374393e4b1662026d7ea30411
Anil Prabkhudesay <prabhudesay.anil@yandex.ru>
2026-06-28 10:44:51 +0700

    updated db.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,21 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-    user: 'postgres',
-    password: '1494',
-    host: 'localhost',
-    port: 5432,
-    database: 'snake_db'
-});
+let pool;
+    if (process.env.DATABASE_URL) {
+        pool = new Pool({
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        });
+    } else {
+        pool = new Pool({
+            user: 'postgres',
+            host: 'localhost',
+            database: 'snake_db',
+            password: '1494',
+            port: 5432,
+        });
+}
 
 module.exports = pool;
